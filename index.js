c9d68cf4e1b0508ec4c16e7b9529ca1cc720edf7
PrinceK001 <tripathi.prince635@gmail.com>
2019-12-17 21:37:31 +0530

    Personal banner updated

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 import './style.css';
-const appDiv = document.getElementById("app");
-appDiv.innerHTML = `<h1 class='title'>Life as a Techie</h1>
-                    <div class = "life">
-                      <ol id="phases"></ol>
-                    </div>`;
+const title = 'Life as a Techie';
 const phases = ['Learn', 'Innovate', 'Repeat'];
+const appDiv = document.getElementById("app");
+appDiv.innerHTML = `<h1 class = 'title'>${title}</h1>
+                    <div class = "life">
+                      <ol id = "phases"></ol>
+                    </div>`;
 const olElem = document.getElementById('phases');
 phases.forEach((phase, i)=>{
   const newEle = document.createElement('li');
-  const newTextEle = document.createTextNode(phase);
-  newEle.appendChild(newTextEle);
+  newEle.textContent = phase;
   olElem.appendChild(newEle);
 });
-
